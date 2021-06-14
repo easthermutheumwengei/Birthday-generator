2c1c46a41fff2ec01d87b34ca5e57adadc82bafd
easthermutheumwengei <easther.mwengei@student.moringaschool.com>
2021-06-14 04:14:41 +0300

    modified script file

--- static/js/script.js
+++ static/js/script.js
@@ -1,67 +1,53 @@
-// let daysArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-// let maleKhanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-// let femaleKhanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+let daysArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let maleKhanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+let femaleKhanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
-// let formValidation = function() {
-//     hideElement();
-//     let dateData = document.forms["khanEntry"]["data"].value
-//     let genderData = document.forms["khanEntry"]["gender"].value
+let formValidation = function() {
+    hideElement();
+    let dateData = document.forms["khanEntry"]["data"].value
+    let genderData = document.forms["khanEntry"]["gender"].value
 
-//     if (dateData == "" || genderData == "") {
-//         unhideElement();
-//         alert("Ensure all inputs are entered correctly to get your Khan name")
-//         return false;
-//     } else {
+    if (dateData == "" || genderData == "") {
+        unhideElement();
+        alert("Ensure all inputs are entered correctly to get your Khan name")
+        return false;
+    } else {
 
-//         getUserDetails();
-//     }
-// }
-// let getUserDetails = function() {
-//     let data = document.getElementById("data").value;
-//     let gender = document.getElementById("gender").value
-//     let genDate = new Date(data).getDay();
+        getUserDetails();
+    }
+}
 
-//     Generate Akan Name(genDate, gender)
-// }
+let getUserDetails = function() {
+    let data = document.getElementById("data").value;
+    let gender = document.getElementById("gender").value
+    let genDate = new Date(data).getDay();
 
-// let display = document.getElementById("display")
+    getAkhanName(genDate, gender)
+}
 
-// let getAkhanName = function(day, gender) {
-//     if (gender === "Male") {
-//         display.innerHTML = `<p>Your Akan name is <strong> ${maleKhanNames[day]}</strong>  becuase you are a ${gender} born on ${daysArray[day]}</p>`
+let display = document.getElementById("display")
 
-//     } else if (gender === "Female") {
-//         display.innerHTML = `<p>Your Akan name is <strong> ${femaleKhanNames[day]} </strong>  becuase you are a ${gender} born on ${daysArray[day]}</p>`
-//     }
-// }
+let getAkhanName = function(day, gender) {
+    if (gender === "Male") {
+        display.innerHTML = `<p>Your Akan name is <strong> ${maleKhanNames[day]}</strong>  becuase you are a ${gender} born on ${daysArray[day]}</p>`
 
-// let hideElement = function() {
-//     var x = document.getElementById("hiden-display");
-//     x.style.display = "block";
+    } else if (gender === "Female") {
+        display.innerHTML = `<p>Your Akan name is <strong> ${femaleKhanNames[day]} </strong>  becuase you are a ${gender} born on ${daysArray[day]}</p>`
+    }
+}
 
-// }
+let hideElement = function() {
+    var x = document.getElementById("hiden-display");
+    x.style.display = "block";
 
-// let unhideElement = function() {
-//     var x = document.getElementById("hiden-display");
-//     x.style.display = "none";
+}
 
-// }
+let unhideElement = function() {
+    var x = document.getElementById("hiden-display");
+    x.style.display = "none";
 
-// let back = function() {
-//     location.reload();
-// }
-let day = ["Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-];
-let maleNames = ["kwasi", "kwako", "kwabena", "kwaku", "yow", "kofi", "kwame"];
-let femaleNames = ["Akosua", "AdWOA", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+}
 
-
-function dayOfBirth() {
-    let
+let reset = function() {
+    location.reload();
 }
